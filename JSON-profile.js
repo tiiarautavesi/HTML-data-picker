27640a3dbda1239d60fe54c773ec6449b509a090
Tiia <tiia.rautavesi@metropolia.fi>
2017-04-03 09:39:54 +0300

    Update JSON-profile.js

--- JSON-profile.js
+++ JSON-profile.js
@@ -1,7 +1,10 @@
 var myProfile = {
   "name":"Tiia",
   "gender":"Female",
-  "music": ["Lisa Mitchell, Leevi and the Leavings, Michael Jackson"]
+  "music": ["Lisa Mitchell, Leevi and the Leavings, Michael Jackson"],
+  "tv": ["Rakas, sinusta on tullut pullukka, MTV Uutiset, Karpolla on asiaa, Digimon"],
+  "movies": ["Mr. Nobody, Matrix, Star Wars, Harry Potter"],
+  "restaurants": ["Fuku, Hesburger, Itis Kebab, Morrison's"]
 }
 
 console.log(JSON.stringify(myProfile));
