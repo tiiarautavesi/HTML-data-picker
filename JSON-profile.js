952dbb3beb17ddb9e1f75b43a9339eb092e4f147
Tiia <tiia.rautavesi@metropolia.fi>
2017-04-03 10:06:37 +0300

    Lisätty: JavaScript objektin printtaus JSON objektina

--- JSON-profile.js
+++ JSON-profile.js
@@ -1,3 +1,5 @@
+/* Ensimmäinen ratkaisu */
+
 var myProfile = {
   "name":"Tiia",
   "gender":"Female",
@@ -8,3 +10,20 @@ var myProfile = {
 }
 
 console.log(JSON.stringify(myProfile));
+
+/* Toinen ratkaisu */
+
+function createElementDotNotation() {
+    var profile = {};
+    profile.nimi = 'Tiia';
+    profile.gender = 'Female';
+    profile.music = ['Lisa Mitchell', 'Leevi and the Leavings', 'Michael Jackson'];
+    profile.tv = ['Rakas, sinusta on tullut pullukka', 'MTV Uutiset', 'Karpolla on asiaa', 'Digimon'];
+    profile.movies = ['Mr. Nobody', 'Matrix', 'Star Wars', 'Harry Potter'];
+    profile.restaurants = ['Fuku', 'Hesburger', 'Itis Kebab', 'Morrison'];
+    
+    return profile;
+}
+
+var element1 = createElementDotNotation();
+console.log(JSON.stringify(element1));
